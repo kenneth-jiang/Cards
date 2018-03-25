e70af16c96109c5ecb0b3d4f341047e051c99696
Kenneth <26774728+kenneth-jiang@users.noreply.github.com>
2018-03-25 15:47:52 -0400

    Finished card class.

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 class Card:
 	def __init__(self, suit, value):
-		if suit in ("Hearts", "Diamonds", "Clubs", or "Spades"):
+		if suit in ("Hearts", "Diamonds", "Clubs", "Spades"):
 			self.suit = suit
 		else:
 			raise ValueError("Suit must be Hearts, Diamonds, Clubs, or Spades")
 		if value in ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"):
 			self.value = value
+		else:
+			raise ValueError("Value must be A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K")
 	def __repr__(self):
 		return f"{self.value} of {self.suit}"
 
